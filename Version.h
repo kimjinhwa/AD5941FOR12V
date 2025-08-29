b26e21c6cdf6b2b32b15063c85bcae787e62f82b
kimjinhwa <mvcjhkim@gmail.com>
2025-08-29 17:08:36 +0900

    modbus까지 완료 하였다.

--- Version.h
+++ Version.h
@@ -1,3 +1,3 @@
-//#define VERSION "2.0.2"  // modbus까지 완료 하였다.
+#define VERSION "2.0.2"  // modbus까지 완료 하였다.
 //#define VERSION "2.0.1"  // IMP 데이타 Reading OK.  
 //#define VERSION "2.0.0"  // New Board designed using easyeda Version 2.0.0 
