5f9cd7143c4b0a90d1abea760dfe630f820604c3
kimjinhwa <mvcjhkim@gmail.com>
2025-09-23 14:44:48 +0900

    main board 3.0에서 새로운 branch를 시작한다.

--- Version.h
+++ Version.h
@@ -1,3 +1,3 @@
-#define VERSION "2.0.2"  // modbus까지 완료 하였다.
+#define VERSION "3.0.0"  // main board 3.0에서 새로운 branch를 시작한다. 
 //#define VERSION "2.0.1"  // IMP 데이타 Reading OK.  
 //#define VERSION "2.0.0"  // New Board designed using easyeda Version 2.0.0 
